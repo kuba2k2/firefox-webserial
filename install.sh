b6506f6fc1cd9f33cb3682ffdb59eeaff2bb5bc7
Virusmater <virusmater@gmail.com>
2024-07-23 23:52:46 +0200

    Remove debug lines

--- install.sh
+++ install.sh
@@ -2,11 +2,6 @@
 
 curl -L -o ~/.mozilla/native-messaging-hosts/firefox-webserial https://github.com/kuba2k2/firefox-webserial/releases/latest/download/firefox-webserial-linux-x86-64
 chmod +x ~/.mozilla/native-messaging-hosts/firefox-webserial
-
-
-
-kernel="2.6.39"
-distro="xyz"
 cat > ~/.mozilla/native-messaging-hosts/io.github.kuba2k2.webserial.json <<EOL
 {
 	"name": "io.github.kuba2k2.webserial",
